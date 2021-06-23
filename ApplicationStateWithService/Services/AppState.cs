e3178528c7178ff84ee6cc8c98144f37e04f1342
sandro.vecchiarelli <sandro.vecchiarelli@pomiager.com>
2021-06-23 13:04:31 +0200

    Commit on 23_06_2021

---
+++ ApplicationStateWithService/Services/AppState.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApplicationStateWithService.Services
+{
+    public class AppState
+    {
+        public string Message { get; private set; } = "";
+        public bool Enabled { get; private set; } = false;
+        public int Counter { get; private set; } = 0;
+
+        public void UpdateMessage(ComponentBase Source, string Message)
+        {
+            this.Message = Message;
+            NotifyStateChanged(Source, "Message");
+        }
+        public void UpdateEnabled(ComponentBase Source, bool Enabled)
+        {
+            this.Enabled = Enabled;
+            NotifyStateChanged(Source, "Enabled");
+        }
+        public void UpdateCounter(ComponentBase Source, int Counter)
+        {
+            this.Counter = Counter;
+            NotifyStateChanged(Source, "Counter");
+        }
+
+        public event Action<ComponentBase, string> StateChanged;
+
+        private void NotifyStateChanged(ComponentBase Source, string Property) => StateChanged?.Invoke(Source, Property);
+
+        //private void NotifyStateChanged(ComponentBase Source, string Property)
+        //{
+        //    if(StateChanged != null)
+        //    {
+        //        //StateChanged.Invoke(Source, Property);
+        //        Delegate[] delegates = StateChanged.GetInvocationList();
+        //        foreach (Delegate d in delegates)
+        //        {
+        //            Action<ComponentBase, string> sc = (Action<ComponentBase, string>)d;
+        //            sc.Invoke(Source, Property);
+
+        //        }
+        //    }
+        //}
+    }
+}
---
+++ PersistingAppState/Services/AppState.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json;
+
+namespace PersistingAppState.Services
+{
+    public class AppState
+    {
+        [JsonProperty]
+        public string Message { get; private set; } = "";
+        [JsonProperty]
+        public bool Enabled { get; private set; } = false;
+        [JsonProperty]
+        public int Counter { get; private set; } = 0;
+        [JsonProperty]
+        public int TimeToLiveInSeconds { get; set; } = 60;
+        [JsonProperty]
+        public DateTime LastAccessed { get; set; } = DateTime.Now;
+
+        public void UpdateMessage(ComponentBase Source, string Message)
+        {
+            this.Message = Message;
+            NotifyStateChanged(Source, "Message");
+        }
+
+        public void UpdateEnabled(ComponentBase Source, bool Enabled)
+        {
+            this.Enabled = Enabled;
+            NotifyStateChanged(Source, "Enabled");
+        }
+
+        public void UpdateCounter(ComponentBase Source, int Counter)
+        {
+            this.Counter = Counter;
+            NotifyStateChanged(Source, "Counter");
+        }
+
+        public event Action<ComponentBase, string> StateChanged;
+
+        private void NotifyStateChanged(ComponentBase Source, string Property) => StateChanged?.Invoke(Source, Property);
+    }
+}
