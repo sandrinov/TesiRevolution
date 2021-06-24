d523c4c12410560749e3e70a0e73db867f3e50b6
sandro.vecchiarelli <sandro.vecchiarelli@pomiager.com>
2021-06-24 12:06:10 +0200

    Added Project gRPC

---
+++ BlazorGrpcDemo/Client/Services/ApiService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using BlazorGrpcDemo.Shared;
+
+namespace BlazorGrpcDemo.Client.Services
+{
+    public class ApiService
+    {
+        HttpClient http;
+
+        public ApiService(HttpClient _http)
+        {
+            http = _http;
+        }
+
+        public async Task<List<Person>> GetAll()
+        {
+            try
+            {
+                var result = await http.GetAsync("persons");
+                result.EnsureSuccessStatusCode();
+                string responseBody = await result.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<List<Person>>(responseBody);
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.Message;
+                return null;
+            }
+        }
+
+        public async Task<Person> GetPersonById(int Id)
+        {
+            try
+            {
+                var result = await http.GetAsync($"persons/{Id}/getbyid");
+                result.EnsureSuccessStatusCode();
+                string responseBody = await result.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<Person>(responseBody);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
